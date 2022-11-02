96e2549da2395af15b4ca7652ac46296ba1a8ed7
Rain <whitee181@gmail.com>
2022-11-02 00:52:04 -0400

    Day 4.2 --- day 5.1

--- Day4/Day4_GiantSquid_Part2.py
+++ Day4/Day4_GiantSquid_Part2.py
@@ -1,3 +1,94 @@
 # Rain White
-# Start:
+# Start: 10/23/2022
 # Finish:
+
+import sys
+
+bingoBoards = []  # BingoBoards[board][row][number]
+numsDrawn = []  # The nums that will be drawn in "random" order, front to back
+highDrawn = ''  # Highest num, that will be drawn
+output = 0
+
+with open("Day4_Test", "r") as inputFile:  # Grabs data from input file and creates "lines" to store it
+    numsDrawn = inputFile.readline().split(',')
+    highDrawn = len(numsDrawn)-1
+
+    bingoBoardsUnform = inputFile.readlines()
+    for x in range(int((len(bingoBoardsUnform))/6)):
+        bingoBoards.append([bingoBoardsUnform[(x * 6)+1][:-1].replace('  ', ' ').strip().split(),
+                            bingoBoardsUnform[(x * 6)+2][:-1].replace('  ', ' ').strip().split(),
+                            bingoBoardsUnform[(x * 6)+3][:-1].replace('  ', ' ').strip().split(),
+                            bingoBoardsUnform[(x * 6)+4][:-1].replace('  ', ' ').strip().split(),
+                            bingoBoardsUnform[(x * 6)+5][:-1].replace('  ', ' ').strip().split()])
+    inputFile.close()
+
+bkey = len(bingoBoards)
+
+
+def bingodump(B, D):  # (Board) : Sorts winning board and outputs sum of (unmarked nums * last drawn num)
+    dump = []
+    out = 0
+    for r in B:
+        for n in r:
+            dump.append(int(n))
+
+    dump.sort()
+    dump = dump[0:dump.index(int(highDrawn)+1)]  # cuts off marked nums
+    print(dump)
+
+    for x in dump:
+        out += x
+    out = out * int(D)
+
+    return out
+
+
+def bingocheck(D, output=0):  # (Recent drawn, 0) : Checks the rows and colums for bingos, outputs sum of unmarked nums * recent draw
+    coless = []
+    global bkey, board
+    for boardC in range(len(bingoBoards)):  # board: 1 space
+        for row in range(5):  # row: 2 space
+
+            colum = []
+            # Debug VVV
+            # print("First: {0} {1} {2}\nSecond: {3} {4} {5}\nDrawn: {6}".format(
+            #     bingoBoards[boardC][row] == [highDrawn + 1] * 5,
+            #     bingoBoards[boardC][row], [highDrawn + 1] * 5,
+            #     colum == [highDrawn + 1] * 5, colum,
+            #     [highDrawn + 1] * 5, D))
+
+            for columx in range(5):  # makes colum
+                colum.append(bingoBoards[boardC][columx][row])
+
+
+            if (bingoBoards[boardC][row] == [highDrawn + 1] * 5) & (bkey != 1):  # checks row for bingo : removes if winning
+                bingoBoards.pop(boardC)
+                bkey -= 1
+                board = boardC = bkey
+
+            if (colum == [highDrawn + 1] * 5) & (bkey != 1):  # checks colum for bingo : removes if winning
+                bingoBoards.pop(boardC)
+                bkey -= 1
+                board = boardC = bkey
+
+            if bkey == 1:  # Checks for last board in list
+                x = bingodump(bingoBoards[boardC], int(numsDrawn[drawn]))
+                print("Board sum * Recent draw: {0}\n{1}".format(x, drawn))
+                sys.exit(bingoBoards[boardC])
+
+
+
+for drawn in range(len(numsDrawn)):                          # drawn: 1 space
+    print(bkey)
+    board=bkey
+    while board > 0:                                # board: 2 space
+        print("board")
+        for row in range(len(bingoBoards[board])):           # row: 3 space
+            print("row")
+            for num in range(len(bingoBoards[board][row])):  # num: 4 space
+                print("beep")
+                if int(bingoBoards[board][row][num]) == int(numsDrawn[drawn]):  # bingo check
+                    print("hit")
+                    bingoBoards[board][row][num] = highDrawn + 1  # marks numbers with the highest possible draw+1
+                print("out")
+                bingocheck(int(numsDrawn[drawn]), 0)
--- Day5/Day5_HydrothermalVenture_Part1.py
+++ Day5/Day5_HydrothermalVenture_Part1.py
@@ -5,32 +5,75 @@
 X = []
 Y = []
 
+global grid
+grid = []
+
 with open("Day5_Test", "r") as inputFile:  # Grabs data from input file and creates "input" to store it
     input = []
     for i in inputFile.readlines():
         input.append(i[:-1].replace(' -> ', ' ').replace(',', ' '))
     inputFile.close()
 
-for i in input:  # splits input into X1 and X2 as well as Y1 and Y2 : breaks input into usable data
+for i in input:  # Splits input into X1 and X2 as well as Y1 and Y2 : breaks input into usable data
     split = i.split()
-    X.append((int(split[0]), int(split[2])))
-    Y.append((int(split[1]), int(split[3])))
+    if (int(split[0]) == int(split[2]) or int(split[1]) == int(
+            split[3])):  # Formats lines so no diagonal lines go through
+        X.append((int(split[0]), int(split[2])))
+        Y.append((int(split[1]), int(split[3])))
 
 firstmaxxsort = list(map(max, X))
-secondmaxxsort = max(firstmaxxsort)
+secondmaxxsort = max(firstmaxxsort)  # Gets max num that'll be used : X
 
 firstmaxysort = list(map(max, Y))
-secondmaxysort = max(firstmaxysort)
+secondmaxysort = max(firstmaxysort)  # Gets max num that'll be used : Y
 
-def fillgrid(L, W):  # fills "grid" with array of 0 [][]
+
+def fillgrid(L, W):  # Fills "grid" with array of 0 [][]
     for x in range(L):
-       grid.append([0 for element in range(W)])
+        grid.append([0 for element in range(W)])
 
-def editgrid(x, y):
-    grid
+
+def IncreaseNum(x, y):  # Increases grid[x][y] number by one until number hits 2
+    if grid[y][x] < 2:
+        grid[y][x] += 1
+
+def Linez(x1, x2, y1, y2, P):  # X1, X2, Y1, Y2, Pointer
+    print(x1, x2, y1, y2, range(y1, y2), range(x1, x2))
+    if x1 == x2:
+        if y1 > y2:
+            for y in range(y1, y2, -1):
+                print(y)
+                IncreaseNum(x1, y)
+        elif y1 < y2:
+            for y in range(y1, y2):
+                print(y)
+                IncreaseNum(x1, y)
+
+    elif y1 == y2:
+        if x1 > x2:
+            for x in range(x1, x2, -1):
+                print(x)
+                IncreaseNum(x, y1)
+        elif x1 < x2:
+            for x in range(x1, x2):
+                print(x)
+                IncreaseNum(x, y1)
+
+    elif (x1 == x2)&(y1 == y2):
+        print("owo")
+
+
 
 def main():
-    print("X: ",X, "\nY: ",Y)
+    fillgrid(secondmaxxsort+1, secondmaxysort+1)
+    for x in range(len(X)):
+        print(x)
+        Linez(X[x][0], X[x][1], Y[x][0], Y[x][1], x)
+
+    for x in range(len(grid)):
+        print(grid[x])
+
+    print()
 
 
 main()
