585ad282f1e0d68e02861b7d71cffe109e122de9
Rain <whitee181@gmail.com>
2022-11-06 21:49:47 -0500

    Day 7.1

--- Day7/Day7_TheTreacheryOfWhales_Part1.py
+++ Day7/Day7_TheTreacheryOfWhales_Part1.py
@@ -1,19 +1,33 @@
 # Rain White
-# Start:
+# Start: 11/5/2022
 # Finish:
+
+from collections import Counter
 
 with open("Day7_Test", "r") as inputFile:
     INPUT = inputFile.readline()
     INPUT = INPUT.split(",")
-    List = [eval(i) for i in INPUT]
+    CrabPos = [eval(i) for i in INPUT]
 
-Length = len(List)
-mean = 0
+C = Counter(CrabPos)
+Fuel = 0
 
-for x in range (Length):
-    mean += List[x]
+for x in range(len(CrabPos)):
+    temp = CrabPos[x] - C.most_common(1)[0][0]
+    if CrabPos[x] - C.most_common(1)[0][0] > 0:
+        Fuel += CrabPos[x] - C.most_common(1)[0][0]
+        print(temp)
+    elif (CrabPos[x] - C.most_common(1)[0][0]) != 0:
+        Fuel += C.most_common(1)[0][0] - CrabPos[x]
+        print(C.most_common(1)[0][0] - CrabPos[x])
+    else:
+        print(0)
 
-print(mean/Length)
 
 
-print(List)
+print(max(CrabPos), min(CrabPos), C)
+
+
+print(CrabPos)
+
+print(Fuel)
